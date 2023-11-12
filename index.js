d2e96fa6afbca3e47db0d412aad80b1d212f89b7
Tatiana_K <ottertut@gmail.com>
2023-11-12 12:50:44 +0200

    web-data

--- index.js
+++ index.js
@@ -44,6 +44,22 @@ if(msg?.web_app_data?.data) {
     }
 }
 });
+app.post('/web-data', async (req, res) => {
+    const {queryId, products = [], totalPrice} = req.body;
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            input_message_content: {
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+            }
+        })
+        return res.status(200).json({});
+    } catch (e) {
+        return res.status(500).json({})
+    }
+})
 // New code
 app.get('/', function (req, res) {
     res.send('the REST endpoint test run!');
