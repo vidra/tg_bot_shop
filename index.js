c14715956749b973960e2e63e780714f3ca66593
Tatiana_K <ottertut@gmail.com>
2023-11-12 18:36:25 +0200

    old

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
+const app = express();
 const fs = require('fs');
-const PORT = 8080;
+const port = 8080;
 const cors = require('cors');
 
+// replace the value below with the Telegram token you receive from <strong i="6">@BotFather</strong>
 const token = '6136917496:AAHyI3XzIpw2wV3GrQo1BtBfZymqXqoX44Q';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
 const webAppUrl = 'https://flourishing-druid-5a80da.netlify.app';
 
-const bot = new TelegramBot(token, {polling: true});
-const app = express();
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
+  const chatId = msg.chat.id;
+  const text = msg.text;
 
-    if(text === '/start') {
-        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
-            reply_markup: {
-                keyboard: [
-                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
-                ]
-            }
-        })
+if(text === '/start') {
+  await bot.sendMessage(chatId, 'Заходи в наш магазин!',{
+    reply_markup: {
+      inline_keyboard: [
+        [{text: 'Заполнить форму', web_app:{url: webAppUrl + '/form'}}]]
+    }
+  })
 
-        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+    await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
             reply_markup: {
                 inline_keyboard: [
                     [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
                 ]
             }
         })
-    }
+}
 
     if(msg?.web_app_data?.data) {
         try {
             const data = JSON.parse(msg?.web_app_data?.data)
-            console.log(data)
-            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+           bot.sendMessage(chatId, 'Спасибо за обратную связь!')
             await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
             await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
 
@@ -51,7 +51,6 @@ bot.on('message', async (msg) => {
         }
     }
 });
-
 app.post('/web-data', async (req, res) => {
     const {queryId, products = [], totalPrice} = req.body;
     try {
@@ -68,3 +67,11 @@ app.post('/web-data', async (req, res) => {
         return res.status(500).json({})
     }
 })
+// New code
+app.get('/', function (req, res) {
+    res.send('the REST endpoint test run!');
+});
+
+app.listen(port, function() {
+  console.log('Server running at http://127.0.0.1:%s', port);
+});
